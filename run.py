1e39afa4ca1f8d7d9ffb7a0d13f71cd99e2e82a0
_Joe <joelin890120@gmail.com>
2023-09-18 17:20:14 +0800

    fix run script & add config_setting.json

--- run.py
+++ run.py
@@ -5,10 +5,12 @@ with open("config_setting.json", encoding="utf-8") as json_file:
     config_setting = json.load(json_file)
 print(config_setting)
 print("Running...........")
-
+run_script = 'blender'
+if config_setting["blender_env"]["background_blender"]:
+    run_script += ' --background'
+if config_setting["file_env"]["blender_file"]:
+    run_script += f' {config_setting["file_env"]["blender_file"]}'
+run_script += f' --python {config_setting["file_env"]["python_file"]}'
+os.system(run_script)
 # os.system(f'blender --background {config_setting["file_env"]["blender_file"]} --python {config_setting["file_env"]["python_file"]}')
-# os.system(f'blender {config_setting["file_env"]["blender_file"]}')
-# os.system(f'blender --python {config_setting["file_env"]["python_file"]}')
-os.system(f'blender --background --python test_BBOX.py')
-
 print("\n\nDone!")
