b53caf2b82c3755e942997364f47725a99424a42
_Joe <joelin890120@gmail.com>
2023-09-18 11:48:43 +0800

    fix camera location & rotation

--- Camera.py
+++ Camera.py
@@ -6,12 +6,12 @@ class Camera:
         self.config_setting = config_setting
 
     def set_camera(self):
-        bpy.data.objects['Camera'].location[0] = 0
-        bpy.data.objects['Camera'].location[1] = 0
-        bpy.data.objects['Camera'].location[2] = 3
-        bpy.data.objects['Camera'].rotation_euler[0] = math.radians(82)
-        bpy.data.objects['Camera'].rotation_euler[1] = 0
-        bpy.data.objects['Camera'].rotation_euler[2] = 0
+        # bpy.data.objects['Camera'].location[0] = 0
+        # bpy.data.objects['Camera'].location[1] = 0
+        # bpy.data.objects['Camera'].location[2] = 3
+        # bpy.data.objects['Camera'].rotation_euler[0] = math.radians(82)
+        # bpy.data.objects['Camera'].rotation_euler[1] = 0
+        # bpy.data.objects['Camera'].rotation_euler[2] = 0
         bpy.data.objects['Camera'].data.clip_end = self.config_setting["blender_env"]["cam_distance"]
         if self.config_setting["mode_config"]["is_fisheye"]:
             bpy.data.objects['Camera'].data.type = 'PANO'
