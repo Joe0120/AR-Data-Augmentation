1b01a22b2fbb8bb14138f208b6dbc05e30a3404e
_Joe <joelin890120@gmail.com>
2024-01-29 20:07:04 +0800

    fix: setting camera

--- Camera.py
+++ Camera.py
@@ -4,16 +4,18 @@ import math
 class Camera:
     def __init__(self, config_setting):
         self.config_setting = config_setting
+        self.location = {"x":None, "y":None, "z":None}
+        self.rotation = {"x":None, "y":None, "z":None}
 
     def set_camera(self):
         if self.config_setting["blender_env"]["cam_location"]:
-            bpy.data.objects['Camera'].location[0] = self.config_setting["blender_env"]["cam_location"][0]
-            bpy.data.objects['Camera'].location[1] = self.config_setting["blender_env"]["cam_location"][1]
-            bpy.data.objects['Camera'].location[2] = self.config_setting["blender_env"]["cam_location"][2]
+            for _idx, key in enumerate(self.location):
+                self.location[key] = self.config_setting["blender_env"]["cam_location"][_idx]
+                bpy.data.objects['Camera'].location[_idx] = self.config_setting["blender_env"]["cam_location"][_idx]
         if self.config_setting["blender_env"]["cam_rotation"]:
-            bpy.data.objects['Camera'].rotation_euler[0] = math.radians(self.config_setting["blender_env"]["cam_rotation"][0])
-            bpy.data.objects['Camera'].rotation_euler[1] = math.radians(self.config_setting["blender_env"]["cam_rotation"][1])
-            bpy.data.objects['Camera'].rotation_euler[2] = math.radians(self.config_setting["blender_env"]["cam_rotation"][2])
+            for _idx, key in enumerate(self.rotation):
+                self.rotation[key] = self.config_setting["blender_env"]["cam_rotation"][_idx]
+                bpy.data.objects['Camera'].rotation_euler[_idx] = self.config_setting["blender_env"]["cam_rotation"][_idx]
 
         bpy.data.objects['Camera'].data.clip_end = self.config_setting["blender_env"]["cam_distance"]
         if self.config_setting["mode_config"]["is_fisheye"]:
